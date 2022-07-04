966b80e783e9f41ffd153b87a76666124230274f
Prajakta Thomas <prajakta.k2206@gmail.com>
2022-07-04 13:25:05 +0530

    initial commit

--- src/master-chef.ts
+++ src/master-chef.ts
@@ -6,65 +6,52 @@ import {
   OwnershipTransferred,
   Withdraw
 } from "../generated/MasterChef/MasterChef"
-import { ExampleEntity } from "../generated/schema"
+import { DepositTxn ,EmergencyWithdrawalTxn,WithdrawTxn } from "../generated/schema"
 
 export function handleDeposit(event: Deposit): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let deposit = DepositTxn.load(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (!deposit) {
+    deposit = new DepositTxn(event.transaction.from.toHex())
+    deposit.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.BONUS_MULTIPLIER(...)
-  // - contract.bonusEndBlock(...)
-  // - contract.devaddr(...)
-  // - contract.getMultiplier(...)
-  // - contract.migrator(...)
-  // - contract.owner(...)
-  // - contract.pendingSushi(...)
-  // - contract.poolInfo(...)
-  // - contract.poolLength(...)
-  // - contract.startBlock(...)
-  // - contract.sushi(...)
-  // - contract.sushiPerBlock(...)
-  // - contract.totalAllocPoint(...)
-  // - contract.userInfo(...)
+  deposit.count = deposit.count.plus(BigInt.fromI32(1)) 
+  deposit.user = event.params.user
+  deposit.pid = event.params.pid
+  deposit.amount = event.params.amount.toBigDecimal()
+  deposit.save()
 }
 
-export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
+export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
+
+  let emergencyTxn = EmergencyWithdrawalTxn.load(event.transaction.from.toHex())
+
+   if (!emergencyTxn) {
+    emergencyTxn = new EmergencyWithdrawalTxn(event.transaction.from.toHex())
+   // entity.count = BigInt.fromI32(0)
+  }
+  emergencyTxn.user = event.params.user
+  emergencyTxn.pid = event.params.pid
+  emergencyTxn.amount = event.params.amount.toBigDecimal()
+  emergencyTxn.save()
+
+  
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleWithdraw(event: Withdraw): void {}
+export function handleWithdraw(event: Withdraw): void {
+
+  let withdraw = WithdrawTxn.load(event.transaction.from.toHex())
+
+   if (!withdraw) {
+    withdraw = new WithdrawTxn(event.transaction.from.toHex())
+  }
+  withdraw.user = event.params.user
+  withdraw.pid = event.params.pid
+  withdraw.amount = event.params.amount.toBigDecimal()
+  withdraw.save()
+
+}
